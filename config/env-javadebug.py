c81435cd2609f1c9c03f8664cb21150459e62737
Shahim Essaid <shahim@essaid.com>
2023-03-02 11:23:54 -0800

    Fixes
    Java debug, undoing elastic index settings that weren't picked up (due to how HAPI does it), tec.

--- config/env-javadebug.py
+++ config/env-javadebug.py
@@ -3,4 +3,4 @@ from os import environ
 # This is to enable JVM debugging when needed.
 environ['HS_HAPI_JAVA_OPTIONS'] = \
     environ['HS_HAPI_JAVA_OPTIONS'] + \
-    f' -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=localhost:8081'
+    f' -agentlib:jdwp=\"transport=dt_socket,server=y,suspend=y,address=*:8081\"'
