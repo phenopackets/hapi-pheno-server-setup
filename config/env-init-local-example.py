d969dd9130bbd0cd2e8d2be4a655cee56c73d58d
Shahim Essaid <shahim@essaid.com>
2023-03-02 11:26:53 -0800

    Updated the env local example to show the repo and cmd variables.

--- config/env-init-local-example.py
+++ config/env-init-local-example.py
@@ -5,3 +5,8 @@ environ['HS_HAPI_HOST'] = '127.0.0.1'
 environ['HS_HAPI_HOST_PUBLIC'] = '127.0.0.1'
 environ['HS_HAPI_PORT'] = '18080'
 environ['HS_HAPI_PORT_PUBLIC'] = '18080'
+
+# Set your desired repo and build command like this:
+environ['HS_HAPI_BUILD_GIT_REPO'] = 'https://github.com/hapifhir/hapi-fhir-jpaserver-starter.git'
+environ['HS_HAPI_BUILD_GIT_REF'] = 'image/v6.4.0'
+environ['HS_HAPI_BUILD_CMD'] = 'mvn -Pboot -DskipTests clean package'
