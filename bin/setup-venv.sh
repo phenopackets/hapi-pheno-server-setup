1d9c8da115e5affff80bba48f408a20737af856c
Shahim Essaid <shahim@essaid.com>
2023-03-30 14:55:00 -0700

    Setup without PG or ES by default,

---
+++ bin/setup-venv.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#set -x
+set -e
+set -u
+set -o pipefail
+set -o noclobber
+shopt -s nullglob
+
+# stack overflow #59895
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+. "${DIR}/.hapisetup"
+
+#"${DIR}/hapi.sh" compose up --build --exit-code-from hapi  --abort-on-container-exit
--- config/env-init.py
+++ config/env-init.py
@@ -1,4 +1,3 @@
 from os import environ
 
-environ['HS_PROFILES'] = environ.get('HS_PROFILES', 'env-defaults,hs-defaults,hs-postgresql,hs-local')
-
+environ['HS_PROFILES'] = environ.get('HS_PROFILES', 'env-defaults,hs-defaults,hs-local')
