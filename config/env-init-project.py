ad08f39d676bbd93ba64f7d800c3e8f8caaf0c83
Shahim Essaid <shahim@essaid.com>
2023-06-05 13:23:53 -0700

    HAPI build points to dev hapi-pheno-server

--- config/env-init-project.py
+++ config/env-init-project.py
@@ -9,3 +9,11 @@
 # The full list of environment variables is in config/env-defaults.py.
 
 import os
+
+# Phenopackets uses this port offset
+os.environ['HS_PORT_OFFSET'] = os.environ.get('HS_PORT_OFFSET', '30000')
+
+os.environ['HS_HAPI_BUILD_GIT_REPO'] = os.environ.get('HS_HAPI_BUILD_GIT_REPO',
+                                               'https://github.com/phenopackets/hapi-pheno-server.git')
+os.environ['HS_HAPI_BUILD_GIT_REF'] = os.environ.get('HS_HAPI_BUILD_GIT_REF',
+                                               'refs/remotes/origin/dev')
